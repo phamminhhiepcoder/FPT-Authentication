15bc59e32f20c4ba8d9f4bb14c5c5821adc0c3c3
Pham Minh Hiep <phamminhhiep0402@gmail.com>
2023-10-25 19:37:36 +0700

    minimize assets

--- src/main/resources/static/js/houses.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".page-number").click(function () {
-        var houseName = $("#houseName").val();
-
-        var pageIndex = $(this).html();
-        window.location = "/houselist/"+pageIndex+"?houseName="+houseName;
-    });
-});
-
